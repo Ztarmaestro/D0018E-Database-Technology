980b1917ff2c886df2af010c744dec4603982a4a
Ztarmaestro <erkkar2@gmail.com>
2016-12-07 15:55:32 +0100

    now all css href etc works

--- webpage/main.go
+++ webpage/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 
 	// Third party packages
 //	"github.com/julienschmidt/httprouter"
@@ -34,7 +35,7 @@ func loggedinHandler(w http.ResponseWriter, r *http.Request) {
 
   pagePath := "static/templates/navbar_logout.html"
 
-	pageTemplate := "static/templates/index.html"
+	pageTemplate := "static/templates/login_index.html"
 
 	if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
 		// Something gnarly happened.
@@ -44,7 +45,7 @@ func loggedinHandler(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	}}
 
-	func loginHandler(w http.ResponseWriter, r *http.Request)  {
+func loginHandler(w http.ResponseWriter, r *http.Request)  {
 
 	// you access the cached templates with the defined name, not the filename
 
@@ -58,103 +59,60 @@ func loggedinHandler(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	}}
 
-func adminLoginHandler(w http.ResponseWriter, r *http.Request)  {
+func adminLoginHandler(w http.ResponseWriter, r *http.Request) {
 
-// you access the cached templates with the defined name, not the filename
+	// you access the cached templates with the defined name, not the filename
 
-pagePath := "static/templates/adminlogin.html"
+	pagePath := "static/templates/adminlogin.html"
 
-if t, err := template.ParseFiles(pagePath); err != nil {
-	// Something gnarly happened.
-	http.Error(w, err.Error(), http.StatusInternalServerError)
-} else {
-	// return to client via t.Execute
-	t.Execute(w, nil)
-}}
+	if t, err := template.ParseFiles(pagePath); err != nil {
+		// Something gnarly happened.
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		// return to client via t.Execute
+		t.Execute(w, nil)
+	}}
 
 func adminPageHandler(w http.ResponseWriter, r *http.Request)  {
 
-// you access the cached templates with the defined name, not the filename
+	// you access the cached templates with the defined name, not the filename
 
-pagePath := "static/templates/adminpage.html"
+	pagePath := "static/templates/adminpage.html"
 
-if t, err := template.ParseFiles(pagePath); err != nil {
-	// Something gnarly happened.
-	http.Error(w, err.Error(), http.StatusInternalServerError)
-} else {
-	// return to client via t.Execute
-	t.Execute(w, nil)
-}}
+	if t, err := template.ParseFiles(pagePath); err != nil {
+		// Something gnarly happened.
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		// return to client via t.Execute
+		t.Execute(w, nil)
+	}}
+
+func checkoutHandler(w http.ResponseWriter, r *http.Request)  {
+
+	// you access the cached templates with the defined name, not the filename
+
+	pagePath := "static/templates/checkout.html"
+
+	if t, err := template.ParseFiles(pagePath); err != nil {
+		// Something gnarly happened.
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		// return to client via t.Execute
+		t.Execute(w, nil)
+	}}
 
 func showroomHandler(w http.ResponseWriter, r *http.Request) {
 
   // you access the cached templates with the defined name, not the filename
 
-  pagePath := "static/templates/navbar_login.html"
+  pagePath := "static/templates/navbar_logout.html"
 
-	//if p.ByName("name") == "ferrari" {
-	if r.URL.Path == "/showroom_nologin/ferrari.html" {
-		pageTemplate := "/static/templates/ferrari.html"
+	data := r.URL.Path
 
-		if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-			// Something gnarly happened.
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			// return to client via t.Execute
-			t.Execute(w, nil)
-		}
-	}
-}
+	// Split on slash.
+    result := strings.Split(data, "/")
 
-/*	if p.ByName("name") == "charger" {
-		pageTemplate := "static/templates/charger.html"
-
-		if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-			// Something gnarly happened.
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			// return to client via t.Execute
-			t.Execute(w, nil)
-		}
-	}
-
-	if p.ByName("name") == "camaro" {
-		pageTemplate := "static/templates/camaro.html"
-
-		if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-			// Something gnarly happened.
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			// return to client via t.Execute
-			t.Execute(w, nil)
-		}
-	}
-
-	if p.ByName("name") == "mustang" {
-		pageTemplate := "static/templates/mustang.html"
-
-		if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-			// Something gnarly happened.
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		} else {
-			// return to client via t.Execute
-			t.Execute(w, nil)
-		}
-	}
-}
-
-*/
-
-	func showroom_nologinHandler(w http.ResponseWriter, r *http.Request) {
-
-	  // you access the cached templates with the defined name, not the filename
-
-	  pagePath := "static/templates/navbar_logout.html"
-
-		//if p.ByName("name") == "ferrari" {
-	  fmt.Println("hej")
-		if r.URL.Path == "/showroom_nologin/ferrari" {	
-			fmt.Println("Inside url path")
+		if result[2] == "ferrari" {
 			pageTemplate := "static/templates/ferrari.html"
 
 			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
@@ -165,33 +123,7 @@ func showroomHandler(w http.ResponseWriter, r *http.Request) {
 				t.Execute(w, nil)
 			}
 		}
-}
-
-/*if p.ByName("name") == "charger" {
-			pageTemplate := "static/templates/charger.html"
-
-			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-				// Something gnarly happened.
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				// return to client via t.Execute
-				t.Execute(w, nil)
-			}
-		}
-
-		if p.ByName("name") == "camaro" {
-			pageTemplate := "static/templates/camaro.html"
-
-			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
-				// Something gnarly happened.
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			} else {
-				// return to client via t.Execute
-				t.Execute(w, nil)
-			}
-		}
-
-		if p.ByName("name") == "mustang" {
+		if result[2] == "mustang" {
 			pageTemplate := "static/templates/mustang.html"
 
 			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
@@ -202,14 +134,91 @@ func showroomHandler(w http.ResponseWriter, r *http.Request) {
 				t.Execute(w, nil)
 			}
 		}
+		if result[2] == "charger" {
+			pageTemplate := "static/templates/charger.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+		}
+		if result[2] == "camaro" {
+			pageTemplate := "static/templates/camaro.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+		}
 }
-*/
+
+func showroom_nologinHandler(w http.ResponseWriter, r *http.Request) {
+
+	// you access the cached templates with the defined name, not the filename
+
+	pagePath := "static/templates/navbar_login.html"
+
+	data := r.URL.Path
+
+	// Split on slash.
+		result := strings.Split(data, "/")
+
+	if result[2] == "ferrari" {
+			pageTemplate := "static/templates/ferrari.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+	}
+	if result[2] == "mustang" {
+			pageTemplate := "static/templates/mustang.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+	}
+	if result[2] == "charger" {
+			pageTemplate := "static/templates/charger.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+	}
+	if result[2] == "camaro" {
+			pageTemplate := "static/templates/camaro.html"
+
+			if t, err := template.ParseFiles(pagePath, pageTemplate); err != nil {
+				// Something gnarly happened.
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				// return to client via t.Execute
+				t.Execute(w, nil)
+			}
+	}
+}
 
 func main() {
 	// Instantiate a new router
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-    
 
 	//Real address for server, change back before pushing to git
 	//bindAddr := "130.240.170.56:8080"
@@ -217,27 +226,26 @@ func main() {
 	//Address for testing server on LAN
 	bindAddr := "127.0.0.1:8000"
 
-//	r := httprouter.New()
-
 	//Handlers for differnt pages
     http.HandleFunc("/", indexHandler)
     http.HandleFunc("/startpage", loggedinHandler)
-    http.HandleFunc("/showroom/:name", showroomHandler)
-   	http.HandleFunc("/showroom_nologin/:name", showroom_nologinHandler)
    	http.HandleFunc("/login", loginHandler)
    	http.HandleFunc("/admin_login", adminLoginHandler)
    	http.HandleFunc("/adminpage", adminPageHandler)
-/*	r.GET("/", indexHandler)
-	r.GET("/startpage", loggedinHandler)
+		http.HandleFunc("/checkout", checkoutHandler)
 
-	r.GET("/showroom/:name", showroomHandler)
-	r.GET("/showroom_nologin/:name", showroom_nologinHandler)
+		http.HandleFunc("/showroom/ferrari", showroomHandler)
+   	http.HandleFunc("/showroom_nologin/ferrari", showroom_nologinHandler)
 
-	r.GET("/login", loginHandler)
+		http.HandleFunc("/showroom/mustang", showroomHandler)
+		http.HandleFunc("/showroom_nologin/mustang", showroom_nologinHandler)
 
-	r.GET("/admin_login", adminLoginHandler)
-	r.GET("/adminpage", adminPageHandler)
-*/
+		http.HandleFunc("/showroom/charger", showroomHandler)
+		http.HandleFunc("/showroom_nologin/charger", showroom_nologinHandler)
+
+		http.HandleFunc("/showroom/camaro", showroomHandler)
+		http.HandleFunc("/showroom_nologin/camaro", showroom_nologinHandler)
+
 	fmt.Println("Server running on", bindAddr)
 	log.Fatal(http.ListenAndServe(bindAddr, nil))
 }
