afbbfe4dbb49461888346d81d04ca5398b93fd0c
Ztarmaestro <erkkar2@gmail.com>
2016-12-15 00:45:15 +0100

    some log

--- webpage/main.go
+++ webpage/main.go
@@ -297,6 +297,8 @@ func addToCart(w http.ResponseWriter, r *http.Request) {
 	//substring[3] contains the customerId
 	//substring[2] contains the ProductName
 	substring := strings.Split(result,"/")
+	log.Printf(substring[3])
+	log.Printf(substring[2])
 
 	   // Grab from the database
     var idCustomers, idProducts, Price, UnitsInStock, ProductAvailable string
@@ -316,7 +318,7 @@ func addToCart(w http.ResponseWriter, r *http.Request) {
     // Search the database for the ProductName provided
     // Insert to cart
     err := db.QueryRow("SELECT idProducts, Price, UnitsInStock, ProductAvailable FROM Products WHERE ProductName=?", substring[2]).Scan(&idProducts, &Price, &UnitsInStock, &ProductAvailable)
-		_, err = db.Exec("INSERT INTO Cart(idCustomers, idProducts, Quantity, TotalPrice) VALUES(?, ?, ?, ?)", substring[2], idCustomers, idProducts, '1', Price)
+		_, err = db.Exec("INSERT INTO Cart(idCustomers, idProducts, Quantity, TotalPrice) VALUES(?, ?, ?, ?)", substring[3], idCustomers, idProducts, '1', Price)
 
 	if err != nil {
 		} else {
