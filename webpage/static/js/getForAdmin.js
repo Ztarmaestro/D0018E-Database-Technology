291bc90683249f6a2f495076986fd6145c4152e9
Ztarmaestro <erkkar2@gmail.com>
2017-01-04 00:16:29 +0100

    now gets review onload

--- webpage/static/js/getForAdmin.js
+++ webpage/static/js/getForAdmin.js
@@ -56,31 +56,3 @@ function DisplayAllData(data){
   	}
  }
 }
-
-function updateData(type) {
-    //Type is the users id that is saved in the session. carmodel is the car that is added to the cart
-    var xhr = typeof XMLHttpRequest != 'undefined'
-      ? new XMLHttpRequest()
-      : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('get',"/everything/"+type, true);
-    xhr.onreadystatechange = function() {
-      var status;
-      var data;
-      var obj;
-      // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
-      if (xhr.readyState == 4) { // `DONE`
-        status = xhr.status;
-        if (status == 200) {
-          data = JSON.parse(xhr.response);
-          // obj = JSON.parse(data)
-          console.log(data)
-          //Sends user to cart
-          window.location = "/checkout";
-
-        } else {
-          console.log("error")
-        }
-      }
-    };
-    xhr.send();
-}
