06bba921f26ef245ceb932ac6d7eb3614ea05103
Ztarmaestro <erkkar2@gmail.com>
2017-01-09 21:30:07 +0100

    forgot ||

--- webpage/main.go
+++ webpage/main.go
@@ -538,7 +538,7 @@ func addReview(w http.ResponseWriter, req *http.Request) {
 	err = db.QueryRow("SELECT idCustomers FROM Review WHERE idProducts=? AND idCustomers=?", idProduct, userId).Scan(idcustomerexists)
 	log.Printf("what do I get back? ", idcustomerexists)
 
-	if idcustomerexists != userId idcustomerexists == ""{
+	if idcustomerexists != userId || idcustomerexists == "" {
 
 				  // Grab everything from the database
 					var idProducts string
