4059734003d6ad606f1a11c2313fdb3a916d3ee9
Martin Persson <martin_persson91@hotmail.com>
2017-01-09 14:27:48 +0100

    cookieremoval errors
    asd

--- webpage/static/js/cookies.js
+++ webpage/static/js/cookies.js
@@ -15,12 +15,13 @@ function deleteCookie(cname, cvalue, exdays){
   var cname = str.charAt(0);
 
   console.log("setcookie");
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*0*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    console.log(cname);
+    //var date = new Date("12/15/1990");
+    //d.setDate(30);
+   // var expires = "expires="+ d.toUTCString();
+    //console.log(cname);
     var cvalue = "";
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    //document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + cvalue + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT" +";path=/";
     window.location="/"
 }
 
